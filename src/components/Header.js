82bf0ef2d4e8723c8b1838ade3ef85b4faa2a83d
Tri D Luu <91616223+triluu03@users.noreply.github.com>
2022-09-06 08:01:27 +0300

    change some about header

--- src/components/Header.js
+++ src/components/Header.js
@@ -41,17 +41,21 @@ const Header = () => {
                 Luu Duc Tri
             </Typography>
 
-            <Typography color='cardTitle.main' variant='h5'>
+            <Typography
+                color='cardTitle.main'
+                variant='h5'
+                fontFamily='monospace'
+            >
                 <b>
                     <TypeAnimation
                         sequence={[
-                            `I'm a student `,
+                            `I'm a student`,
                             1000,
-                            `I'm a self-learner `,
+                            `I'm a self-learner`,
                             1000,
-                            `I'm a full stack developer `,
+                            `I'm a full stack developer`,
                             1000,
-                            `I'm a programmer `,
+                            `I'm a programmer`,
                             1000,
                         ]}
                         repeat={Infinity}
