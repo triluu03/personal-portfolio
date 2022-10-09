e32326ae3813327c017d69fa8a46cfc0c5ac76b4
Tri D Luu <91616223+triluu03@users.noreply.github.com>
2022-10-09 12:22:16 +0300

    Change design of Foote

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,26 @@
-import { Box, Typography } from '@mui/material'
-
-import GitHubIcon from '@mui/icons-material/GitHub'
-import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import { Box, Typography, Link } from '@mui/material'
 
 const Footer = () => {
+    const styles = {
+        footerContainer: {
+            width: 100,
+            height: 'auto',
+            flexGrow: 1,
+            ml: '2%',
+        },
+    }
+
     return (
         <Box
             sx={{
                 width: '100vw',
                 height: 'auto',
+                minHeight: '30vh',
                 bgcolor: 'primary.main',
                 display: 'flex',
                 flexDirection: 'column',
-                pt: '2%',
-                pb: '2%',
+                pt: '1%',
+                pb: '4%',
             }}
             id='contact'
         >
@@ -21,45 +28,63 @@ const Footer = () => {
                 sx={{
                     display: 'flex',
                     justifyContent: 'center',
-                    width: '100%',
+                    mt: '2%',
+                    ml: '9%',
                 }}
             >
-                <Typography variant='h3' color='textPrimary.main'>
-                    Get in Touch
-                </Typography>
+                <Box sx={{ ...styles.footerContainer }}>
+                    <Typography color='textSecondary.main'>Email</Typography>
+                    <Typography color='textPrimary.main'>
+                        tri.luu@helsinki.fi
+                    </Typography>
+                    <Typography color='textPrimary.main'>
+                        triluu.1802@gmail.com
+                    </Typography>
+                </Box>
+                <Box sx={{ ...styles.footerContainer }}>
+                    <Typography color='textSecondary.main'>Connect</Typography>
+                    <Typography color='textPrimary.main'>
+                        <Link
+                            href='https://www.linkedin.com/in/triluu03/'
+                            target='_blank'
+                            color='textPrimary.main'
+                            sx={{
+                                ':hover': { color: 'cardTitle.main' },
+                            }}
+                        >
+                            LinkedIn
+                        </Link>
+                    </Typography>
+                    <Typography color='textPrimary.main'>
+                        <Link
+                            href='https://github.com/triluu03'
+                            target='_blank'
+                            color='textPrimary.main'
+                            sx={{
+                                ':hover': { color: 'cardTitle.main' },
+                            }}
+                        >
+                            GitHub
+                        </Link>
+                    </Typography>
+                </Box>
+                <Box sx={{ ...styles.footerContainer }}>
+                    <Typography color='textSecondary.main'>Phone</Typography>
+                    <Typography color='textPrimary.main'>
+                        (+358) 46 902 1807
+                    </Typography>
+                </Box>
             </Box>
-            <Box sx={{ mt: '1.5%', textAlign: 'center' }}>
-                <Typography>
-                    <a
-                        href='https://www.linkedin.com/in/triluu03/'
-                        target='_blank'
-                        rel='noreferrer'
-                    >
-                        <LinkedInIcon
-                            color='textPrimary'
-                            sx={{
-                                fontSize: '5em',
-                                ':hover': {
-                                    color: 'cardTitle.main',
-                                },
-                            }}
-                        />
-                    </a>{' '}
-                    <a
-                        href='https://github.com/triluu03'
-                        target='_blank'
-                        rel='noreferrer'
-                    >
-                        <GitHubIcon
-                            color='textPrimary'
-                            sx={{
-                                fontSize: '5em',
-                                ':hover': {
-                                    color: 'cardTitle.main',
-                                },
-                            }}
-                        />
-                    </a>
+            <Box
+                sx={{
+                    width: '100%',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    pt: '7%',
+                }}
+            >
+                <Typography color='textSecondary.main' sx={{ mt: '2%' }}>
+                    © This website is built all by Tri Luu
                 </Typography>
             </Box>
         </Box>
